ccf8d03d1eb9f3f9b83c7446623c1899f977e43b
gibran abdillah <unkownuser145@gmail.com>
2020-09-02 14:59:29 +0700

    add feature

--- core/modules/controllers.py
+++ core/modules/controllers.py
@@ -1,6 +1,18 @@
 from core import *
-from .data import *
+from core.modules.data import *
 
 @app.route('/')
 def index_page():
     return render_template('index.html')
+
+
+@app.route('/add-siswa',methods=["POST","GET"])
+
+def add_data():
+    if request.method == 'POST':
+        nama = request.form['nama']
+        kelas = request.form['kelas']
+        anjg = data_siswa(nama=nama,kelas=kelas)
+        db.session.add(anjg)
+        db.session.commit()
+        return 'Sukses'
--- core/modules/data.py
+++ core/modules/data.py
@@ -1,13 +1,17 @@
 from flask_sqlalchemy import SQLAlchemy
-from core import *
+from core import app
 
 
 db = SQLAlchemy(app)
 
 
 class data_siswa(db.Model):
-    __tablename__ = 'siswa'
+
+    __tablename__ = 'datasiswa'
 
     id = db.Column(db.Integer,primary_key=True,nullable=False)
     nama = db.Column(db.String(64),index=True)
-    kelas = db.Column(db.String(5))
+    kelas = db.Column(db.String(10))
+
+    def __repr__(self):
+        return '<nama:{}'.format(self.nama)
